37157e23023b1da7c711de368ef759519940eb83
Andrea Facchin <afacchin73@gmail.com>
2018-03-13 17:20:43 +0100

    Aggiornamento Script SQL
    Ho aggiornato lo script sql per la creazione del db con dati di esempio

--- CPLibrary/documentation/databaseCP.sql
+++ CPLibrary/documentation/databaseCP.sql
@@ -32,15 +32,22 @@ CREATE TABLE IF NOT EXISTS `cpanagra` (
   KEY `cpa_campagid` (`cpa_cpgid`),
   KEY `cpa_rifTerzo` (`cpa_rifter`),
   KEY `cpa_rifPratica` (`cpa_rifpra`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='Tabella Anagrafiche delle Campagne Telefoniche del CP\r\nUn record per ogni anagrafica Cliente di Pratica di Recupero Crediti che abbia almeno 1 n. di telefono registrato\r\nAlla Tabella Anagra sono collegate le tabelle Telefoni e Chiamate ';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='Tabella Anagrafiche delle Campagne Telefoniche del CP\r\nUn record per ogni anagrafica Cliente di Pratica di Recupero Crediti che abbia almeno 1 n. di telefono registrato\r\nAlla Tabella Anagra sono collegate le tabelle Telefoni e Chiamate ';
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella cprovider.cpanagra: ~3 rows (circa)
+/*!40000 ALTER TABLE `cpanagra` DISABLE KEYS */;
+INSERT INTO `cpanagra` (`cpa_id`, `cpa_cpgid`, `cpa_rifter`, `cpa_rifpra`, `cpa_nome`, `cpa_numpty`, `cpa_calsts`, `cpa_calcnt`, `cpa_numpho`, `cpa_curpho`) VALUES
+	(1, 2, '970413', '88988744561235', 'Roberta Brusa', 1, 1, 0, 2, 0),
+	(2, 2, '970412', '87889977661701', 'Andrea Facchin', 0, 1, 0, 2, 0),
+	(5, 3, '950200', '86888433454896', 'Marco Moret', 0, 1, 0, 1, 0);
+/*!40000 ALTER TABLE `cpanagra` ENABLE KEYS */;
+
 -- Dump della struttura di tabella cprovider.cpcalls
 CREATE TABLE IF NOT EXISTS `cpcalls` (
   `cpl_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'chiave primaria log chiamate',
   `cpl_cpaid` bigint(20) DEFAULT NULL COMMENT 'id tabella Anagrafica',
   `cpl_numpho` int(2) DEFAULT NULL COMMENT 'n progressivo di telefono della anagrafica: da 1 a n dove n = cpa_numpho',
-  `cpl_datcall` char(8) DEFAULT NULL COMMENT 'Data chiamata',
+  `cpl_seqcall` char(8) DEFAULT NULL COMMENT 'Data chiamata',
   `cpl_oracall` char(6) DEFAULT NULL COMMENT 'Ora inizio chiamata',
   `cpl_calsts` int(1) DEFAULT NULL COMMENT '1=Da chiamare; 2=Chiamata OK; 3=Chiamata KO; 9=Chiamata in Corso;....',
   `cpl_censts` int(1) DEFAULT NULL COMMENT 'stato risposta centralino: da questo deriviamo il campo sopra, stato',
@@ -48,9 +55,17 @@ CREATE TABLE IF NOT EXISTS `cpcalls` (
   `cpl_caldur` int(3) DEFAULT NULL COMMENT 'Durata della chiamata in secondi (se c''è stata una conversazione)',
   `cpl_agent` varchar(200) DEFAULT NULL COMMENT 'Nome operatore in caso di risposta ok e passaggio chiamata all''operatore',
   PRIMARY KEY (`cpl_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Tabella Chiamate effettuate dal centralino';
+) ENGINE=InnoDB AUTO_INCREMENT=183 DEFAULT CHARSET=latin1 COMMENT='Tabella Chiamate effettuate dal centralino';
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella cprovider.cpcalls: ~4 rows (circa)
+/*!40000 ALTER TABLE `cpcalls` DISABLE KEYS */;
+INSERT INTO `cpcalls` (`cpl_id`, `cpl_cpaid`, `cpl_numpho`, `cpl_seqcall`, `cpl_oracall`, `cpl_calsts`, `cpl_censts`, `cpl_oraEnd`, `cpl_caldur`, `cpl_agent`) VALUES
+	(179, 2, 1, '20180312', '181519', 2, 2, '181519', 0, ''),
+	(180, 1, 1, '20180312', '181519', 3, 3, '181520', 1, ''),
+	(181, 1, 2, '20180312', '181520', 2, 2, '181520', 0, ''),
+	(182, 5, 1, '20180312', '181520', 2, 2, '181520', 0, '');
+/*!40000 ALTER TABLE `cpcalls` ENABLE KEYS */;
+
 -- Dump della struttura di tabella cprovider.cpcamp
 CREATE TABLE IF NOT EXISTS `cpcamp` (
   `cpg_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'chiave',
@@ -62,9 +77,15 @@ CREATE TABLE IF NOT EXISTS `cpcamp` (
   `cpg_status` int(1) NOT NULL COMMENT '0=Disabled; 1=Enabled; 2=Completed; 8=Stop Request Utente; 9=Stopped (in questo status il CP interrompe la sua attività a passa l''EOF al primo getContact che riceve)',
   PRIMARY KEY (`cpg_id`),
   UNIQUE KEY `cpg_campagna` (`cpg_campag`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='Tabella Campagne telefoniche Contact Provider (CP)\r\nUn record per ogni campagna inserita su Phones\r\nFa da Owner alle altre tabelle (anagrafiche e telefoni) \r\n';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='Tabella Campagne telefoniche Contact Provider (CP)\r\nUn record per ogni campagna inserita su Phones\r\nFa da Owner alle altre tabelle (anagrafiche e telefoni) \r\n';
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella cprovider.cpcamp: ~2 rows (circa)
+/*!40000 ALTER TABLE `cpcamp` DISABLE KEYS */;
+INSERT INTO `cpcamp` (`cpg_id`, `cpg_campag`, `cpg_descri`, `cpg_type`, `cpg_maxcal`, `cpg_calitv`, `cpg_status`) VALUES
+	(2, 'BIFIS1701C1', 'Primo contatto Banca Ifis affido 1701', 'I', 4, 3, 0),
+	(3, 'XXXXXXXXXX', 'test', 'I', 2, 5, 0);
+/*!40000 ALTER TABLE `cpcamp` ENABLE KEYS */;
+
 -- Dump della struttura di tabella cprovider.cpphones
 CREATE TABLE IF NOT EXISTS `cpphones` (
   `cpp_cpaid` int(11) NOT NULL DEFAULT 0,
@@ -76,7 +97,16 @@ CREATE TABLE IF NOT EXISTS `cpphones` (
   PRIMARY KEY (`cpp_cpaid`,`cpp_numpho`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Tabella Telefoni delle Anagrafiche delle Campagne del CP\r\nUn record per ogni numero telefonico della Anagrafica\r\nFa riferimento alla tabella Anagrafica';
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella cprovider.cpphones: ~5 rows (circa)
+/*!40000 ALTER TABLE `cpphones` DISABLE KEYS */;
+INSERT INTO `cpphones` (`cpp_cpaid`, `cpp_numpho`, `cpp_phonum`, `cpp_calsts`, `cpp_censts`, `cpp_calcnt`) VALUES
+	(1, 1, '3394154717', 1, 0, 0),
+	(1, 2, '3334444444', 1, 0, 0),
+	(2, 1, '3381999809', 1, 0, 0),
+	(2, 2, '3389999999', 1, 0, 0),
+	(5, 1, '3335656561', 1, 0, 0);
+/*!40000 ALTER TABLE `cpphones` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
